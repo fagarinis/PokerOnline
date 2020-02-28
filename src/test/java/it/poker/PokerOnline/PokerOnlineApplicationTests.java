71b60c57d3b47e17120dabd2fb6d9544cdade8c3
Federico Agarinis <federicof1994@gmail.com>
2020-02-28 16:23:28 +0100

    primo commit

---
+++ src/test/java/it/poker/PokerOnline/PokerOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package it.poker.PokerOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
